b5a4c1ae6ea7892a16808b9e3b40ce11d8afc78d
Tony Su <peitaosu@163.com>
2018-11-14 23:02:12 +0800

    init project

---
+++ dan.py
@@ -0,0 +1,9 @@
+class Dan():
+
+    def __init__(self):
+        self.site = None
+        self.room = None
+    
+    def get_dan(self, queue):
+        pass
+
---
+++ gamer.py
@@ -0,0 +1,11 @@
+class Gamer():
+
+    def __init__(self):
+        self.game = None
+        self.ops_mapping = {}
+
+    def set_ops_mapping(self):
+        pass
+
+    def exec_ops(self, queue):
+        pass
